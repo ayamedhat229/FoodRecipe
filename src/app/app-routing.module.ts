d483fec2da9b03ee2bebea32ac09c96bbbcc9584
ayamedhat229 <ayamedhat773@gmail.com>
2024-03-27 23:18:44 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [{path:'',redirectTo:'auth',pathMatch:'full'},
 { path: 'dashboard', canActivate:[AuthGuard], loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
